877eb6cd44f2be2a25a3d0de60fa6658f72510e4
Brian McGowan <bmcgowan@shutterstock.com>
2024-03-13 22:51:13 +0000

    adding viewMode.launch

--- app/src/main/java/com/mcgowanb/projects/refereescorekeeper/model/GameViewModel.kt
+++ app/src/main/java/com/mcgowanb/projects/refereescorekeeper/model/GameViewModel.kt
@@ -1,17 +1,30 @@
 package com.mcgowanb.projects.refereescorekeeper.model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mcgowanb.projects.refereescorekeeper.action.ScoreAction
 import com.mcgowanb.projects.refereescorekeeper.enums.Team
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class GameViewModel : ViewModel() {
 
+    //    problem number 1, initializes the state flow as a blank object every time
+    // if I could replace this with the persisted state I'd be laughing
     private val _uiState = MutableStateFlow(GameState())
     val uiState: StateFlow<GameState> = _uiState.asStateFlow()
+
+//    val uiState = mutableStateOf(GameState())
+//        init {
+    //this should potentially be where we load our data from the data store
+//            viewModelScope.launch {
+//                uiState.value = GameState()
+//                // Coroutine that will be canceled when the ViewModel is cleared.
+//            }
+//        }
 
 
     fun onAction(action: ScoreAction) {
@@ -30,51 +43,63 @@ class GameViewModel : ViewModel() {
 
     private fun addGoal(team: Team) {
         if (team == Team.HOME) {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    hGoals = _uiState.value.hGoals.plus(1)
-                )
+            viewModelScope.launch {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        hGoals = _uiState.value.hGoals.plus(1)
+                    )
+                }
             }
         } else {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    aGoals = _uiState.value.aGoals.plus(1)
-                )
+            viewModelScope.launch {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        aGoals = _uiState.value.aGoals.plus(1)
+                    )
+                }
             }
         }
     }
 
     private fun addPoint(team: Team) {
         if (team == Team.HOME) {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    hPoints = _uiState.value.hPoints.plus(1)
-                )
+            viewModelScope.launch {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        hPoints = _uiState.value.hPoints.plus(1)
+                    )
+                }
             }
         } else {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    aPoints = _uiState.value.aPoints.plus(1)
-                )
+            viewModelScope.launch {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        aPoints = _uiState.value.aPoints.plus(1)
+                    )
+                }
             }
         }
     }
 
     private fun subtractGoal(team: Team) {
         if (team == Team.HOME) {
-            if (_uiState.value.hGoals > 0)
-                _uiState.update { currentState ->
-                    currentState.copy(
-                        hGoals = _uiState.value.hGoals.minus(1)
-                    )
-                }
+            viewModelScope.launch {
+                if (_uiState.value.hGoals > 0)
+                    _uiState.update { currentState ->
+                        currentState.copy(
+                            hGoals = _uiState.value.hGoals.minus(1)
+                        )
+                    }
+            }
         } else {
-            if (_uiState.value.aGoals > 0)
-                _uiState.update { currentState ->
-                    currentState.copy(
-                        aGoals = _uiState.value.hGoals.minus(1)
-                    )
-                }
+            viewModelScope.launch {
+                if (_uiState.value.aGoals > 0)
+                    _uiState.update { currentState ->
+                        currentState.copy(
+                            aGoals = _uiState.value.hGoals.minus(1)
+                        )
+                    }
+            }
         }
     }
 
