17cc05ce81c9be3d220cd4e432842987c1a7731f
Brian McGowan <bmcgowan@shutterstock.com>
2024-09-11 22:22:21 +0100

    fixing state on reload

--- app/src/main/java/com/mcgowanb/projects/refereescorekeeper/model/GameState.kt
+++ app/src/main/java/com/mcgowanb/projects/refereescorekeeper/model/GameState.kt
@@ -41,5 +41,43 @@ data class GameState(
         }
 
     private fun Int.twoDigitFormat(): String = "%02d".format(this)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GameState
+
+        if (hPoints != other.hPoints) return false
+        if (hGoals != other.hGoals) return false
+        if (aPoints != other.aPoints) return false
+        if (aGoals != other.aGoals) return false
+        if (_totalHomeScoreInt != other._totalHomeScoreInt) return false
+        if (_totalAwayScoreInt != other._totalAwayScoreInt) return false
+        if (homeScore != other.homeScore) return false
+        if (awayScore != other.awayScore) return false
+        if (totalHomeScore != other.totalHomeScore) return false
+        if (totalAwayScore != other.totalAwayScore) return false
+        if (homeDiff != other.homeDiff) return false
+        if (awayDiff != other.awayDiff) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = hPoints
+        result = 31 * result + hGoals
+        result = 31 * result + aPoints
+        result = 31 * result + aGoals
+        result = 31 * result + _totalHomeScoreInt
+        result = 31 * result + _totalAwayScoreInt
+        result = 31 * result + homeScore.hashCode()
+        result = 31 * result + awayScore.hashCode()
+        result = 31 * result + totalHomeScore.hashCode()
+        result = 31 * result + totalAwayScore.hashCode()
+        result = 31 * result + homeDiff.hashCode()
+        result = 31 * result + awayDiff.hashCode()
+        return result
+    }
+
 
 }
