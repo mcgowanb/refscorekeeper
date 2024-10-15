f1fdb0158884215ba165705c7345e7c397cde0e9
Brian McGowan <bmcgowan@shutterstock.com>
2024-10-16 00:57:26 +0100

    preview

--- app/src/main/java/com/mcgowanb/projects/refereescorekeeper/ui/main/ScoreDisplayBox.kt
+++ app/src/main/java/com/mcgowanb/projects/refereescorekeeper/ui/main/ScoreDisplayBox.kt
@@ -1,5 +1,7 @@
 package com.mcgowanb.projects.refereescorekeeper.ui.main
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,8 +10,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.Text
+import com.mcgowanb.projects.refereescorekeeper.model.GameTimeViewModel
+import com.mcgowanb.projects.refereescorekeeper.model.GameViewModel
 
 @Composable
 fun ScoreDisplayBox(
@@ -31,4 +36,11 @@ fun ScoreDisplayBox(
             text = text
         )
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Preview(device = "id:wearos_small_round", showSystemUi = true)
+@Composable
+fun ScoreDisplayBoxPreview() {
+   ScoreDisplayBox(text = "blah", verticalAlignment = Alignment.Center)
 }
